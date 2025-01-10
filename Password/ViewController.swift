fcabf88f73d359251fee62f6e65dec9394553a94
Samuel <yusheng593@gmail.com>
2025-01-10 14:48:22 +0800

    Initial commit

---
+++ Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by yusheng Lu on 2025/1/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
